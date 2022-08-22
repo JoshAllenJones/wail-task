aa949b57489913bb095ca0e89e614a43b905bdcd
Josh <joshallenjones@gmail.com>
2022-08-22 17:58:32 -0400

    a whole mess of stuff

--- frontend/src/Components/TaskEntry.jsx
+++ frontend/src/Components/TaskEntry.jsx
@@ -1,23 +1,42 @@
-import { Card, Grid, Paper, TextInput, Text } from '@mantine/core';
+import { Card, Grid, Paper, TextInput, Text, Button } from "@mantine/core";
+import taskListAtom from '../atoms/taskListAtom'
+import {useRecoilState} from 'recoil'
 
+function TaskEntry(props) {
 
-function TaskEntry(props){
+    const [taskListState, setTaskListState] = useRecoilState(taskListAtom)
+  
+    let handleEnter = (event) => {
+        if(event.key == 'Enter'){
+            console.log('Wooh!')
+        }
+    }
+
+    let submitHandler = (event) => {
+        console.log('WOAAasdasdSDASDASDO!')
+    }
     
+  
     return (
-        <Paper>
-            <Grid>
-                <Grid.Col span={10}>
-                    <Grid>
-                        <Grid.Col span={12}><TextInput /> </Grid.Col>
-                    </Grid>
-                    
-                </Grid.Col>
-                <Grid.Col span={2}>
-                    <Text>Okay</Text>
-                </Grid.Col>
-            </Grid>
-        </Paper>
-    )
+    <Paper shadow="xs" p="md">
+    <form onSubmit={submitHandler}>
+      <Grid>
+        <Grid.Col span={10}>
+          <Grid>
+            <Grid.Col span={12}>
+              <TextInput sx={{input: {border: "None"}}}  onKeyDown={handleEnter} />
+            </Grid.Col>
+          </Grid>
+        </Grid.Col>
+        <Grid.Col span={2}>
+          <Button variant="gradient"  type="submit"  gradient={{ from: "indigo", to: "cyan" }} fullWidth>
+            Submit
+          </Button>
+        </Grid.Col>
+      </Grid>
+      </form>
+    </Paper>
+  );
 }
 
-export default TaskEntry
+export default TaskEntry;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import {createRoot} from 'react-dom/client'
 import './style.css'
 import App from './App'
+import { MantineProvider } from '@mantine/core'
+ import {RecoilRoot} from 'recoil';
 
 const container = document.getElementById('root')
 
 const root = createRoot(container)
 
 root.render(
-    <React.StrictMode>
-            <App/>
-    </React.StrictMode>
+        <RecoilRoot>
+            <MantineProvider>
+                <App/>
+            </MantineProvider>
+        </RecoilRoot>
 )
